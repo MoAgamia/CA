7f22359ded2387c4ee50564f7a14c0ff68e0fcd5
RainbowZephyr <darkfeathers9@gmail.com>
2015-04-11 00:02:13 +0200

    gui action listener finished

--- reg.rb
+++ reg.rb
@@ -4,51 +4,49 @@ class Reg
 	@@RFormatArray = ["add", "sub", "nor", "sll", "srl", "and", "jr", "slt", "sltu"]
 	@@JFormatArray = ["j", "jal"]
 
-	def check (command)
+	def self.check command
 		if command == nil
 			puts ("Error empty command")
 		end
 
-		commandArray = command.split(",")
+		commandSplit = command.split
 
-
-		case commandArray[0]
-			when @@IFormatArray.include?(commandArray[0])
-				CheckIFormat(command)
-			when @@RFormatArray.include?(commandArray[0])
-				CheckRFormat(command)
-			when @@JFormatArray.include?(commandArray[0])
-				CheckJFormat(command)
-			else
-				puts ("Please write your command correctly")
+		
+		if(@@IFormatArray.include? commandSplit[0].strip)
+			self.checkIFormat command
+		elsif (@@RFormatArray.include? commandSplit[0].strip)
+			self.checkRFormat command
+		elsif (@@JFormatArray.include? commandSplit[0].strip)
+			self.checkJFormat command
 		end
+		
 	end
 
-	def CheckIFormat (command)
-        if command.match(/^(\s*)(\w{2} | \w{3} | \w{4})(\s+)(\$\w)(\d |\d{2})(\s*),(\s*)((\$\w)(\d|\d{2})(\s*) | (\$\w+)(\s*)),(\s*)((\d+) | (\w+))(\s*)$/) || x.match(/^(\s*)(\w{2} | \w{3} | \w{4})(\s+)(\$\w)(\d |\d{2})(\s*),(\s*)(\d+)(\()(\s*)(\$\w)(\d|\d{2})(\s*)(\))(\s*)$/)
+	def self.checkIFormat command
+        if command.match(/^(\s*)(\w{2}|\w{3}|\w{4})(\s+)(\$\w)(\d |\d{2})(\s*),(\s*)((\$\w)(\d|\d{2})(\s*) | (\$\w+)(\s*)),(\s*)((\d+) | (\w+))(\s*)$/) || x.match(/^(\s*)(\w{2} | \w{3} | \w{4})(\s+)(\$\w)(\d |\d{2})(\s*),(\s*)(\d+)(\()(\s*)(\$\w)(\d|\d{2})(\s*)(\))(\s*)$/)
 			return true
 		else
 			return false
 		end
 	end
 
-	def CheckRFormat (command)
-    	if command.match(/^\s*(\w{2} | \w{3} | \w{4})(\s+)(\$\w)(\d|\d{2})(\s*),(\s*)((\$\w)(\d|\d{2}) | (\$\w+))(\s*),(\s*)((\$\w)(\d|\d{2}) | (\$\w+))(\s*)$/)
+	def self.checkRFormat command
+    	if command.match(/^\s*(\w{2}|\w{3}|\w{4})(\s+)(\$\w)(\d|\d{2})(\s*),(\s*)((\$\w)(\d|\d{2}) | (\$\w+))(\s*),(\s*)((\$\w)(\d|\d{2}) | (\$\w+))(\s*)$/)
 			return true
 		else
 			return false
 		end
 	end
 
-	def CheckJFormat (command)
-		if command.match(/^(\s*)(\w | \w{3})(\s+)(\w+)(\s*)$/)
+	def self.checkJFormat command
+		if command.match(/^(\s*)(j|jal)(\s+)(\w+)(\s*)$/)
 			return true
 		else
 			return false
 		end
 	end
-	
-	
-	
+
+	 puts self.check("jr $t3")
 
 end
+
--- test.rb
+++ test.rb
@@ -1,6 +1,29 @@
-x = "j Loop"
-if x.match(/^(\s*)(\w|\w{3})(\s+)(\w+)(\s*)$/)
-  puts true
-else
-  puts false
-end
+require 'java'
+
+ java_import 'java.awt.event.ActionListener'
+ java_import 'javax.swing.JButton'
+ java_import 'javax.swing.JFrame'
+
+ class ClickAction
+  include ActionListener
+
+  def actionPerformed(event)
+    puts "Button got clicked."
+  end
+ end
+
+ class MainWindow < JFrame
+   def initialize
+     super "JRuby/Swing Demo"
+     setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
+
+     button = JButton.new( "Click me!") do |e|
+    
+      end
+     button.addActionListener ClickAction.new
+     add button
+     pack
+   end
+ end
+
+ MainWindow.new.setVisible true
