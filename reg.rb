a5f4f736e482d5c04d84df786dfb20c11491f39f
RainbowZephyr <darkfeathers9@gmail.com>
2015-04-12 04:07:58 +0200

    fixed regexps 2

--- reg.rb
+++ reg.rb
@@ -1,6 +1,7 @@
 class Reg
 
 	@@IFormatArray = ["lw", "addi", "lb", "lbu", "sw", "sb", "lui", "beq", "bne"]
+	@@conditions = ["addi", "bne", "beq"]
 	@@RFormatArray = ["add", "sub", "nor", "and", "sll", "srl", "slt", "sltu", "jr"]
 	@@shifts = ["sll", "srl"]
 	@@JFormatArray = ["j", "jal"]
@@ -13,8 +14,10 @@ class Reg
 		commandSplit = command.split
 
 
-		if(@@IFormatArray.include? commandSplit[0].strip)
-			self.checkIFormat command
+		if((@@IFormatArray.include? commandSplit[0].strip ) && (@@conditions.include? commandSplit[0]))
+			self.checkIConditions command
+		elsif(@@IFormatArray.include? commandSplit[0].strip)
+			self.checkIFormat command			
 		elsif ((@@RFormatArray.include? commandSplit[0].strip) && (@@shifts.include? commandSplit[0].strip)) 
 			self.checkRShift command
 		elsif ((@@RFormatArray.include? commandSplit[0].strip) && (commandSplit[0].strip == "jr"))
@@ -23,12 +26,22 @@ class Reg
 			self.checkRFormat command
 		elsif (@@JFormatArray.include? commandSplit[0].strip)
 			self.checkJFormat command
+		else
+			return "Command Not Found: #{commandSplit[0].strip}"
 		end
 
 	end
 
 	def self.checkIFormat command
-        if command.match(/^(\s*)(\w{3,4})(\s+)(\$([a-z]{1,2}\d?)|zero)(\s*,\s*)(\$([a-z]{1,2}\d?)|zero)(\s*,\s*)(\d+)(\s*)$/) || command.match(/^(\s*)([a-z]{2,3})(\s+)(\$([a-z]{1,2}\d?)|zero)(\s*,\s*)(\d+)(\((\$([a-z]{1,2}\d?)|zero)\))$/)
+        if  command.match(/^(\s*)([a-z]{2,3})(\s+)(\$([a-z]{1,2}\d?)|zero)(\s*,\s*)(\d+)(\((\$([a-z]{1,2}\d?)|zero)\))(\s*)$/)
+			return true
+		else
+			return false
+		end
+	end
+	
+	def self.checkIConditions command
+		if command.match(/^(\s*)(\w{3,4})(\s+)(\$([a-z]{1,2}\d?)|zero)(\s*,\s*)(\$([a-z]{1,2}\d?)|zero)(\s*,\s*)(\d+)(\s*)$/)
 			return true
 		else
 			return false
@@ -36,7 +49,7 @@ class Reg
 	end
 	
 	def self.checkRFormat command
-    	if command.match(/^(\s*)([a-z]{2,4})(\s+)(\$(([a-z]{1,2}\d?)|zero))(\s*,\s*)(\$(([a-z]{1,2}\d?)|zero))(\s*,\s*)(\$(([a-z]{1,2}\d?)|zero))(\s*)$/)
+	    	if command.match(/^(\s*)([a-z]{2,4})(\s+)(\$(([a-z]{1,2}\d?)|zero))(\s*,\s*)(\$(([a-z]{1,2}\d?)|zero))(\s*,\s*)(\$(([a-z]{1,2}\d?)|zero))(\s*)$/)	
 			return true
 		else
 			return false
@@ -67,9 +80,10 @@ class Reg
 			return false
 		end
 	end
-
-	puts self.check("add $t1, $t0, $t6")
-	puts self.check("add $t1, $t0 $t6")
-	puts self.check("add $t1, $t0 ,0")
+	
+	
+	puts self.check("addi $t1, $t0 , 8 ")	
+	
+	puts self.check("lbu $t1, $t0 , 8 ")
 
 end
